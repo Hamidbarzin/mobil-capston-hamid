f5929ed1461436edde361006b94914f2f4287a3a
Hamidbarzin <Hamidreza.Zebardastbarzin@triosstudent.com>
2024-06-07 22:14:46 -0400

    added file

---
+++ topping app/topping app/informationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  informationViewController.swift
+//  topping app
+//
+//  Created by Hamidreza Zebardast on 2024-06-07.
+//
+
+import UIKit
+
+class informationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ topping app/topping app/loginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  topping app
+//
+//  Created by Hamidreza Zebardast on 2024-06-07.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
